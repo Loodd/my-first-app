9f5005016e57a95ac897bf26ed034d5eb3d8bd87
Federico Laurianti <laurianti@gmail.com>
2022-08-17 00:40:31 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,35 @@
 import { Probot } from "probot";
+import { MD5 } from "crypto-js";
+
+const commands = require('probot-commands')
 
 export = (app: Probot) => {
   app.on("issues.opened", async (context) => {
+    var owner = "0xD7C91D12c9Ace617eC2F2B20803dB8E166585baE";
+
+    var fundingPoolId = MD5(JSON.stringify({
+      context: 'GitHub',
+      repositoryId: context.payload.repository.id,
+      issueNumber: context.payload.issue.number
+    }));
+
+    var urlSvg = "http://80.180.103.122:3001";
+    var urlFunder = `http://80.180.103.122:4200/web3/funding/${owner}/${fundingPoolId}?ref=${context.payload.issue.html_url}`;
 
     const issueComment = context.issue({
-      body: `Thanks for opening this issue! ${JSON.stringify(context)}`
+      body: `[![alt text](${urlSvg}/?owner=${owner}&fundingPoolId=${fundingPoolId})](${urlFunder})`
     });
 
     await context.octokit.issues.createComment(issueComment);
   });
 
-  app.on('issue_comment.created', async (context) => {
+  commands(app, 'label', async (context: any, command: any) => {
+    const labels = command.arguments.split(/, */);
+
     const issueComment = context.issue({
-      body: `Addres: ${JSON.stringify(context.payload)}`
+      body: `Addres: ${JSON.stringify(labels)}`
     });
 
     await context.octokit.issues.createComment(issueComment);
-
-    var matcher = /^\/([\w]+)\b *(.*)?$/m;
-
-    var message = context.payload.comment;
-
-    const command = message.body.match(matcher);
-
-    if (command != null && command[1] == "address") {
-      const issueComment = context.issue({
-        body: `Addres: ${JSON.stringify(command)}`
-      });
-  
-      await context.octokit.issues.createComment(issueComment);
-    }
   });
 };
