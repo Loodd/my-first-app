afdbe2641f35e055c4a6cb490f74d2857bca62f0
Federico Laurianti <laurianti@gmail.com>
2022-08-25 01:28:43 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { Probot } from "probot";
 import { MD5 } from "crypto-js";
+import { Contract, ethers } from "ethers";
+import { OpenSourceTokenAbi } from "./contracts/OpenSourceTokenAbi";
+import abi = require('../abis/OpenSourceToken.abi.json');
 
 const commands = require('probot-commands')
 
@@ -13,8 +16,8 @@ export = (app: Probot) => {
       issueNumber: context.payload.issue.number
     }));
 
-    var urlSvg = "http://80.180.103.122:3001";
-    var urlFunder = `http://80.180.103.122:4200/web3/funding/${owner}/${fundingPoolId}?ref=${context.payload.issue.html_url}`;
+    var urlSvg = "http://80.180.105.139:3001";
+    var urlFunder = `http://80.180.105.139:4200/web3/funding/${owner}/${fundingPoolId}?ref=${context.payload.issue.html_url}`;
 
     const issueComment = context.issue({
       body: `[![alt text](${urlSvg}/?owner=${owner}&fundingPoolId=${fundingPoolId})](${urlFunder})`
@@ -23,8 +26,44 @@ export = (app: Probot) => {
     await context.octokit.issues.createComment(issueComment);
   });
 
-  commands(app, 'label', async (context: any, command: any) => {
+  commands(app, 'claim', async (context: any, command: any) => {
+		const provider = new ethers.providers.JsonRpcProvider("https://zksync2-testnet.zksync.dev");
+
+    const signer = new ethers.Wallet("0x6d6352f3144cc7a9ba24d6f7b603baf05cc556d5bb2de494c39ed473db50f074", provider);
+
+		var contract = new Contract(
+      "0x745c8671B6A6bDE6FB74Ee04D93bBB643E6cd4B5",
+      abi,
+      signer
+		) as OpenSourceTokenAbi;
+
+    var query = `{
+      repository(name: "my-first-app", owner: "Loodd") {
+        pullRequest(number: ${context.payload.issue.number}) {
+          closingIssuesReferences(first: 100) {
+            nodes {
+              number
+            }
+          }
+        }
+      }
+    }`;
+
     const labels = command.arguments.split(/, */);
+
+    context.octokit.graphql(query).then(async (data: any) => {
+      for (var node of data.repository.pullRequest.closingIssuesReferences.nodes) {
+        var fundingPoolId = MD5(JSON.stringify({
+          context: 'GitHub',
+          repositoryId: context.payload.repository.id,
+          issueNumber: node.number
+        }));
+
+        let changeTx = await contract.approveFund(fundingPoolId.toString(), labels[0]);
+
+        await changeTx.wait();
+      }
+    });
 
     const issueComment = context.issue({
       body: `Addres: ${JSON.stringify(labels)}`
