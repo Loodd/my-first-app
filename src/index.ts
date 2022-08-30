89b07059a7768dcb8f8a404adbf6f6e7fa9338b4
Federico Laurianti <laurianti@gmail.com>
2022-08-30 02:07:33 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -13,38 +13,114 @@ var createFundingPoolId = (repositoryId: number, issueNumber: number) => {
   })).toString();
 }
 
+var createIssuesMapping = async (context: any, payload: any) => {
+  var query = `{
+    repository(name: "${context.payload.repository.name}", owner: "${context.payload.repository.owner.login}") {
+      pullRequest(number: ${payload.number}) {
+        closingIssuesReferences(first: 100) {
+          nodes {
+            number
+          }
+        }
+      }
+    }
+  }`;
+
+  var data = await context.octokit.graphql(query);
+  
+  var issuesMapping: any = [];
+
+  for (var node of data.repository.pullRequest.closingIssuesReferences.nodes) {
+    var fundingPoolId = createFundingPoolId(context.payload.repository.id, node.number);
+
+    issuesMapping.push({
+      nodeNumber: node.number,
+      fundingPoolId: fundingPoolId
+    });
+  }
+
+  return issuesMapping;
+}
+
+var createBodyFromIssuesMapping = async (context: any, payload: any) => {
+  var issuesMapping = await createIssuesMapping(context, payload);
+
+  var body = `[//]: <> (%RECAP_COMMENT%)`;
+  body += "\n";
+  body += "\n";
+  body += "| Issue number | SVG |";
+  body += "\n";
+  body += "| ------------- | ------------- |";
+
+  for (var issuesMapping of issuesMapping) {
+    body += "\n";
+    body += "|";
+    body += `#${issuesMapping.nodeNumber}`;
+    body += "|";
+    body += createSvg(issuesMapping.fundingPoolId, payload.html_url);
+    body += "|";
+  }
+
+  return body;
+}
+
+var createSvg = (fundingPoolId: string, url: string) => {
+  var owner = "0xD7C91D12c9Ace617eC2F2B20803dB8E166585baE";
+
+  var urlSvg = "http://80.180.103.134:3001";
+  var urlFunder = `http://80.180.103.134:4200/web3/funding/${owner}/${fundingPoolId}?ref=${url}`;
+
+  return `[![alt text](${urlSvg}/?owner=${owner}&fundingPoolId=${fundingPoolId})](${urlFunder})`;
+}
+
 export = (app: Probot) => {
   app.on("issues.opened", async (context) => {
-    var owner = "0xD7C91D12c9Ace617eC2F2B20803dB8E166585baE";
 
     var fundingPoolId = createFundingPoolId(context.payload.repository.id, context.payload.issue.number);
 
-    var urlSvg = "http://80.180.105.139:3001";
-    var urlFunder = `http://80.180.105.139:4200/web3/funding/${owner}/${fundingPoolId}?ref=${context.payload.issue.html_url}`;
-
     const issueComment = context.issue({
-      body: `[![alt text](${urlSvg}/?owner=${owner}&fundingPoolId=${fundingPoolId})](${urlFunder})`
+      body: createSvg(fundingPoolId, context.payload.issue.html_url)
     });
 
     await context.octokit.issues.createComment(issueComment);
   });
 
-  app.on(["pull_request.opened", "pull_request.edited"], async (context) => {
-    // var comments = await context.octokit.issues.listComments({
-    //   issue_number: context.payload.number,
-    //   owner: context.payload.repository.owner.login,
-    //   repo: context.payload.repository.name
-    // });
+  app.on("pull_request.opened", async (context) => {
+    var body = await createBodyFromIssuesMapping(context, context.payload.pull_request);
 
-    // await context.octokit.issues.updateComment({
-    //   comment_id: 1,
-    //   body: `ciao1`,
-    //   repo: context.payload.repository.name,
-    //   owner: context.payload.repository.owner.login
-    // });
-  })
+    const issueComment = context.issue({
+      body: body
+    });
+
+    await context.octokit.issues.createComment(issueComment);
+  });
+
+  app.on("pull_request.edited", async (context) => {
+    var body = await createBodyFromIssuesMapping(context, context.payload.pull_request);
+
+    const app = await context.octokit.apps.getAuthenticated();
+
+    var comments = await context.octokit.issues.listComments({
+      issue_number: context.payload.number,
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name
+    });
+
+    var c = comments.data
+      .filter(c => c.user?.login == app.data.slug + '[bot]')
+      .filter(c => (c.body?.indexOf("%RECAP_COMMENT%") || -1) > -1)[0];
+    
+    await context.octokit.issues.updateComment({
+      comment_id: c.id,
+      body: body,
+      repo: context.payload.repository.name,
+      owner: context.payload.repository.owner.login
+    });
+  });
 
   commands(app, 'claim', async (context: any, command: any) => {
+    var owner = "0xD7C91D12c9Ace617eC2F2B20803dB8E166585baE";
+
     if (context.payload.comment.user.id !== context.payload.issue.user.id) {
       await context.octokit.issues.createComment(context.issue({
         body: `You can't claim because you are not the owner of this pull request.`
@@ -61,8 +137,6 @@ export = (app: Probot) => {
       return;
     }
 
-    var owner = "0xD7C91D12c9Ace617eC2F2B20803dB8E166585baE";
-
 		const provider = new ethers.providers.JsonRpcProvider("https://zksync2-testnet.zksync.dev");
 
     const signer = new ethers.Wallet("0x6d6352f3144cc7a9ba24d6f7b603baf05cc556d5bb2de494c39ed473db50f074", provider);
@@ -73,28 +147,14 @@ export = (app: Probot) => {
       signer
 		) as OpenSourceTokenAbi;
 
-    var query = `{
-      repository(name: "${context.payload.repository.name}", owner: "${context.payload.repository.owner.login}") {
-        pullRequest(number: ${context.payload.issue.number}) {
-          closingIssuesReferences(first: 100) {
-            nodes {
-              number
-            }
-          }
-        }
-      }
-    }`;
+    var issuesMapping = await createIssuesMapping(context, context.payload.issue);
 
     const labels = command.arguments.split(/, */);
 
-    var issuesMapping: any = {};
-
-    var data = await context.octokit.graphql(query);
-    
-    for (var node of data.repository.pullRequest.closingIssuesReferences.nodes) {
+    for (var issue of issuesMapping) {
       var q = `{
         repository(name: "${context.payload.repository.name}", owner: "${context.payload.repository.owner.login}") {
-          issue(number: ${node.number}) {
+          issue(number: ${issue.nodeNumber}) {
             timelineItems(itemTypes: CLOSED_EVENT, last: 1) {
               nodes {
                 ... on ClosedEvent {
@@ -109,20 +169,15 @@ export = (app: Probot) => {
           }
         }
       }`;
-
+  
       var d = await context.octokit.graphql(q);
-
+  
       if (d.repository.issue.timelineItems.nodes[0].closer?.number !== context.payload.issue.number)
         continue;
 
-      var fundingPoolId = createFundingPoolId(context.payload.repository.id, node.number);
+      issue.balance = await contract.balanceOfFund(owner, issue.fundingPoolId);
 
-      var balance = await contract.balanceOfFund(owner, fundingPoolId);
-
-      issuesMapping[node.number] = balance;
-
-      let changeTx = await contract.approveFund(fundingPoolId, labels[0]);
-
+      let changeTx = await contract.approveFund(issue.fundingPoolId, labels[0]);
       await changeTx.wait();
     }
 
